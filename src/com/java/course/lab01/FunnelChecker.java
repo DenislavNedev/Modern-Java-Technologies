5bda27522321fcbc749a3e60b642450f319078ae
DenislavNedev <dnedev98@gmail.com>
2019-10-16 10:16:34 +0300

    Resolved 01-funnel-checker
    Created solution for the first task 01-funnel-checker

--- src/com/java/course/lab01/FunnelChecker.java
+++ src/com/java/course/lab01/FunnelChecker.java
@@ -2,6 +2,22 @@ package com.java.course.lab01;
 
 public class FunnelChecker {
     public static boolean isFunnel(String str1, String str2) {
+        if (str1.length() - str2.length() != 1) {
+            return false;
+        }
+
+        for (int i = 0; i < str1.length(); i++) {
+            String newStr1 = deleteCharFromString(str1, i);
+            if (newStr1.equals(str2)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    private static String deleteCharFromString(String currentString, int position) {
+        StringBuilder stringWithoutOneChar = new StringBuilder(currentString);
+        stringWithoutOneChar.deleteCharAt(position);
+        return stringWithoutOneChar.toString();
     }
 }
